66e8c16de68c53e1fd1607d580a61cfc76750365
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2026-06-01 01:48:53 +0200

    Update deploy path to talk.antihq.com

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,7 +1,7 @@
 @servers(['web' => 'calm-cliff', 'localhost' => '127.0.0.1'])
 
 @setup
-    $path = '2026-05-26-talk.antihq.com/';
+    $path = 'talk.antihq.com/';
     $branch = 'main';
 @endsetup
 
